fd3cb732a34585e56c6e8cc3de29143bf85a6135
MicheleRai <rainonemichele1999@gmail.com>
2022-11-24 12:48:38 +0100

    24/11

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <TitoloPagina/>
+    <h1>Titolo in app</h1>
+    <TitoloPagina/>
+    <TitoloPagina/>
+    <TitoloPagina/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import TitoloPagina from '@/components/TitoloPagina.vue';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld,
+    TitoloPagina,
   },
 };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style>
+
 </style>
---
+++ src/components/TitoloPagina.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Ciao a tutti dal componente</h1>
+</template>
+
+<script>
+export default {
+  nome: 'TitoloPagina',
+};
+</script>
+
+<style lang="scss" scoped>
+$title_color: red;
+  h1{
+    color: $title_color;
+  };
+</style>
